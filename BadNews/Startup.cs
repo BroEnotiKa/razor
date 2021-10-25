c6c3a62ab8579ed24ace86bf74ca8946e9586377
d.stukov <d.stukov@skbkontur.ru>
2021-10-25 18:17:41 +0500

    Task 1.3

--- BadNews/Startup.cs
+++ BadNews/Startup.cs
@@ -14,6 +14,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using Serilog;
 
 namespace BadNews
 {
@@ -48,6 +49,8 @@ namespace BadNews
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            
+            app.UseSerilogRequestLogging();
             
             app.UseStatusCodePagesWithReExecute("/StatusCode/{0}");
 
