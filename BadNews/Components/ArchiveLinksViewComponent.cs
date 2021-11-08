9ec5e3eefb029240f48460b6ebbc4aec31098b41
Matvey Kazaikin <mv.kazaikin@skbkontur.ru>
2021-11-08 18:32:57 +0500

    Task 4.1

---
+++ BadNews/Components/ArchiveLinksViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using BadNews.Repositories.News;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Components
+{
+    public class ArchiveLinksViewComponent : ViewComponent
+    {
+        private INewsRepository NewsRepository { get; }
+
+        public ArchiveLinksViewComponent(INewsRepository newsRepository)
+        {
+            NewsRepository = newsRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var years = NewsRepository.GetYearsWithArticles();
+            return View(years);
+        }
+    }
+}
