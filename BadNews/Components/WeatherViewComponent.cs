570cde09d5d1102ec04640856e4145f928aaa394
d.stukov <d.stukov@skbkontur.ru>
2021-11-08 18:38:23 +0500

    fix 4.1 and task 4.2 done

---
+++ BadNews/Components/WeatherViewComponent.cs
@@ -0,0 +1,22 @@
+using System.Threading.Tasks;
+using BadNews.Repositories.Weather;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadNews.Components
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        private readonly IWeatherForecastRepository repository;
+
+        public WeatherViewComponent(IWeatherForecastRepository repository)
+        {
+            this.repository = repository;
+        }
+        
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var weatherForecast = await repository.GetWeatherForecastAsync();
+            return View(weatherForecast);
+        }
+    }
+}
