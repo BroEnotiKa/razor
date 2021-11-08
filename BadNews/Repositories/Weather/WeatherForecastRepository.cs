ad3e21393edc9adc1e5af07c93397ae62a40f1dd
Matvey Kazaikin <mv.kazaikin@skbkontur.ru>
2021-11-08 18:45:22 +0500

    Task 4.3

--- BadNews/Repositories/Weather/WeatherForecastRepository.cs
+++ BadNews/Repositories/Weather/WeatherForecastRepository.cs
@@ -1,26 +1,37 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace BadNews.Repositories.Weather
 {
     public class WeatherForecastRepository : IWeatherForecastRepository
     {
-        private const string defaultWeatherImageUrl = "/images/cloudy.png";
+        private const string DefaultWeatherImageUrl = "/images/cloudy.png";
 
-        private readonly Random random = new Random();
+        private Random Random { get; } = new Random();
+        private OpenWeatherClient OpenWeatherClient { get; }
+
+        public WeatherForecastRepository(IOptions<OpenWeatherOptions> weatherOptions)
+        {
+            OpenWeatherClient = new OpenWeatherClient(weatherOptions?.Value.ApiKey);
+        }
 
         public async Task<WeatherForecast> GetWeatherForecastAsync()
         {
+            var openWeatherForecast = await OpenWeatherClient.GetWeatherFromApiAsync();
+            if (openWeatherForecast != null)
+                return WeatherForecast.CreateFrom(openWeatherForecast);
+
             return BuildRandomForecast();
         }
 
         private WeatherForecast BuildRandomForecast()
         {
-            var temperature = random.Next(-20, 20 + 1);
+            var temperature = Random.Next(-20, 20 + 1);
             return new WeatherForecast
             {
                 TemperatureInCelsius = temperature,
-                IconUrl = defaultWeatherImageUrl
+                IconUrl = DefaultWeatherImageUrl
             };
         }
     }
